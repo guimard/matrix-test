bc40d60765f10a3c883e19a75fff03a95a94d140
Yadd <yadd@debian.org>
2023-03-03 07:39:46 +0400

    Switch database to PostgreSQL

--- init.sh
+++ init.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-set -x
-
-mkdir -p synapse-data
+set -xe
 
 MYUID=`perl -e 'print $<'`
+DBCONTNAME=temp-db
+
+# Configure synapse database
+
+docker stop $DBCONTNAME || true
+docker rm $DBCONTNAME || true
+
+ARGS="-e POSTGRES_PASSWORD='synapse!!' -v `pwd`/synapse-db:/var/lib/postgresql/data postgres:13-bullseye"
+
+docker run --entrypoint="" $ARGS chown -R $MYUID /var/lib/postgresql/data
+
+rm -rf synapse-db
+mkdir synapse-db
+
+# Configure synapse
+
+mkdir -p synapse-data
 
 ARGS="--rm -it -v `pwd`/synapse-data:/data -e SYNAPSE_SERVER_NAME=matrix.example.com -e SYNAPSE_REPORT_STATS=no matrixdotorg/synapse:latest"
 
